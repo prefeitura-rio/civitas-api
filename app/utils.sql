375a46a5e08f2e6891f46c4f37c211c0826eaa37
d116626 <d116626@gmail.com>
2024-06-07 16:47:38 -0300

    feat: add new duplicate plates query

--- app/utils.sql
+++ app/utils.sql
@@ -126,7 +126,24 @@ ORDER BY duplicate_occurrences_hour DESC
 
 
 
+-- PLATES OVER TIME
 
+SELECT 
+  empresa,
+  SUBSTR(SHA256(
+        CONCAT(
+            '',
+            SAFE_CAST(REGEXP_REPLACE(placa, r'\.0$', '')  AS STRING)
+        )
+    ), 2,17) as  id,
+  tipoveiculo,
+  velocidade,
+  camera_numero,
+  CONCAT(camera_latitude, camera_longitude) AS position,
+  DATETIME(datahora, "America/Sao_Paulo") AS datahora,
+  COUNT(*) AS duplicates,
+FROM `rj-cetrio.ocr_radar.readings_*`
+GROUP BY 1,2,3,4,5,6,7
 
 
 
