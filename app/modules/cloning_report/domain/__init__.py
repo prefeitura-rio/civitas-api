5127df8cd9fa683264b277bd08d7823c8ab5af94
Gabriel Seixas <salesseixasgabriel@gmail.com>
2025-09-24 14:19:16 -0300

    Refactor imports to use absolute paths in cloning_report

--- app/modules/cloning_report/domain/__init__.py
+++ app/modules/cloning_report/domain/__init__.py
@@ -1,6 +1,6 @@
 """Domain entities and business logic"""
 
-from .entities import CloningReport
-from .detection import Detection, SuspiciousPair
+from app.modules.cloning_report.domain.entities import CloningReport
+from app.modules.cloning_report.domain.detection import Detection, SuspiciousPair
 
-__all__ = ['CloningReport', 'Detection', 'SuspiciousPair']
+__all__ = ["CloningReport", "Detection", "SuspiciousPair"]
