be7a628624d7798c94e2b2d7fb8335d985419b52
nicolasevilasio <nicolas_evilasio@hotmail.com>
2026-04-17 20:00:38 -0300

    feat: enable collection-points endpoint

--- app/config/prod.py
+++ app/config/prod.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from . import getenv_list_or_action, getenv_or_action
 from .base import *  # noqa: F401, F403
 
@@ -20,7 +19,7 @@ if getenv_or_action("TIMEZONE", action="ignore"):
 ALLOWED_ORIGINS = getenv_list_or_action("ALLOWED_ORIGINS", action="ignore")
 ALLOWED_ORIGINS_REGEX = getenv_or_action("ALLOWED_ORIGINS_REGEX", action="ignore")
 if not ALLOWED_ORIGINS and not ALLOWED_ORIGINS_REGEX:
-    raise EnvironmentError("ALLOWED_ORIGINS or ALLOWED_ORIGINS_REGEX must be set.")
+    raise OSError("ALLOWED_ORIGINS or ALLOWED_ORIGINS_REGEX must be set.")
 ALLOWED_METHODS = getenv_list_or_action("ALLOWED_METHODS", action="raise")
 ALLOWED_HEADERS = getenv_list_or_action("ALLOWED_HEADERS", action="raise")
 ALLOW_CREDENTIALS = (
@@ -48,8 +47,15 @@ GCP_SERVICE_ACCOUNT_CREDENTIALS = getenv_or_action(
 )
 
 # feature flags
-ENABLE_GCS_ENDPOINTS = getenv_or_action("ENABLE_GCS_ENDPOINTS", default="false").lower() == "true"
-ENABLE_CLONING_REPORT_ENDPOINT = getenv_or_action("ENABLE_CLONING_REPORT_ENDPOINT", default="false").lower() == "true"
+ENABLE_GCS_ENDPOINTS = (
+    getenv_or_action("ENABLE_GCS_ENDPOINTS", default="false").lower() == "true"
+)
+ENABLE_CLONING_REPORT_ENDPOINT = (
+    getenv_or_action("ENABLE_CLONING_REPORT_ENDPOINT", default="false").lower()
+    == "true"
+)
 
-ENABLE_NEW_RADAR_LIST_ENDPOINT = getenv_or_action("ENABLE_NEW_RADAR_LIST_ENDPOINT", default="false").lower() == "true"
+ENABLE_NEW_RADAR_LIST_ENDPOINT = (
+    getenv_or_action("ENABLE_NEW_RADAR_LIST_ENDPOINT", default="true").lower() == "true"
+)
 GCS_BUCKET_NAME = getenv_or_action("GCS_BUCKET_NAME")
